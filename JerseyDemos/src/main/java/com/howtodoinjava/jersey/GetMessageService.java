602cadd5965883c2b3e7b93eff54897a7b2a924d
Pankaj Bagul <pankaj.bagul@hmrc.gis.gov.uk>
2016-11-01 16:14:34 +0000

    Initial commit

---
+++ JerseyDemos/src/main/java/com/howtodoinjava/jersey/GetMessageService.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.howtodoinjava.jersey;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author pankajbagul
+ *
+ */
+	
+@Path("/restapp")	
+public class GetMessageService {
+
+	
+	HelloWorld helloWorldImpl = new HelloWorldImpl();
+	
+	@GET
+	@Path("/getmessage")
+	public Response getMessage() {
+		String result = helloWorldImpl.getMessage("Pankaj");
+		return Response.status(200).entity(result).build();
+	}
+	
+	public void setHelloWorldImpl(HelloWorld helloWorldImpl) {
+		this.helloWorldImpl = helloWorldImpl;
+	}
+	
+
+}
---
+++ JerseyDemos/src/main/java/com/howtodoinjava/jersey/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.howtodoinjava.jersey;
+
+/**
+ * @author pankajbagul
+ *
+ */
+public interface HelloWorld {
+	String getMessage(String name);
+
+}
