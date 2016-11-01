0ef8dd56128de49616766522e07b27fe4f5cc04a
Pankaj Bagul <pankaj.bagul@hmrc.gis.gov.uk>
2016-11-01 11:17:58 +0000

    Initial commit

---
+++ JerseyDemos/src/main/java/com/howtodoinjava/jersey/HelloWorldImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.howtodoinjava.jersey;
+
+/**
+ * @author pankajbagul
+ *
+ */
+public class HelloWorldImpl implements HelloWorld {
+
+	/* (non-Javadoc)
+	 * @see com.restapp.HelloWorld#getMessage(java.lang.String)
+	 */
+	public String getMessage(String name) {
+		// TODO Auto-generated method stub
+		return "Hello hi " + name;
+	}
+
+}
